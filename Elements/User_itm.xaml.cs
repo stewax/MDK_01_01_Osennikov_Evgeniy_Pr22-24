b6180ea8231dba97f26c49a53cef4bf1f9096252
Evgeniy <sbravl637@gmail.com>
2026-02-06 00:32:47 +0500

    Разработанная страница Call_win.Осенников

---
+++ Elements/User_itm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhoneBook_Осенников.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для User_itm.xaml
+    /// </summary>
+    public partial class User_itm : UserControl
+    {
+        public User_itm()
+        {
+            InitializeComponent();
+        }
+    }
+}
