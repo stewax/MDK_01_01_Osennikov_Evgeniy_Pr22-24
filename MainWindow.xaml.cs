b5cb988ea5a19ca4bb12988d81177c84f9faaa96
Evgeniy <sbravl637@gmail.com>
2026-02-05 23:07:59 +0500

    Разработанный словарь

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace PhoneBook_Осенников
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Connetion connect;
         public MainWindow()
         {
             InitializeComponent();
