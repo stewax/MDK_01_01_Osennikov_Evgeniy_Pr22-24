b846679b3c471476a166feefc8352a76020b64e9
Evgeniy <sbravl637@gmail.com>
2026-02-05 23:19:25 +0500

    Разработанные классы.

---
+++ ClassModule/Call.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassModule
+{
+    public class Call
+    {
+        public int id {  get; set; }
+        public int user_id { get; set; }
+        public int category_call { get; set; }
+        public string date { get; set; }
+        public string time_start { get; set; }
+        public string time_end { get; set; }
+    }
+}
---
+++ ClassModule/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassModule
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string phone_num { get; set; }
+        public string fio_user { get; set; }
+        public string passport_data { get; set; }
+    }
+}
