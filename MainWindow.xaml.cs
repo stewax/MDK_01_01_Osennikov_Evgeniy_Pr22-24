ede4bd48f1b5490dc0398a35bd08cc0136fdedc0
Evgeniy <sbravl637@gmail.com>
2026-02-05 23:12:07 +0500

    Разработанное окно MainWindow.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,10 +21,36 @@ namespace PhoneBook_Осенников
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static Connetion connect;
+        public static Connection connect;
+        public static Pages.Main main;
         public MainWindow()
         {
             InitializeComponent();
+            connect = new Connection();
+            connect.LoadData(Connection.tables.users);
+            connect.LoadData(Connection.tables.calls);
+            main = new Pages.Main();
+            OpenPageMain();
+        }
+
+        public void OpenPageMain()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame.Navigate(main);
+
+                DoubleAnimation opgrisAnimation = new DoubleAnimation();
+                opgrisAnimation.From = 0;
+                opgrisAnimation.To = 1;
+                opgrisAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityProperty, opgrisAnimation);
+            };
+
+            frame.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
         }
     }
 }
