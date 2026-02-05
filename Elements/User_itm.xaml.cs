83d14fded6c0afb301969a83b9977fd9ce4da729
Evgeniy <sbravl637@gmail.com>
2026-02-06 00:40:55 +0500

    Разработанная страница User_itm

--- Elements/User_itm.xaml.cs
+++ Elements/User_itm.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClassModule;
+using PhoneBook_Осенников.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +23,65 @@ namespace PhoneBook_Осенников.Elements
     /// </summary>
     public partial class User_itm : UserControl
     {
-        public User_itm()
+        User user_loc;
+
+        public User_itm(User _user)
         {
             InitializeComponent();
+            user_loc = _user;
+            if (_user.fio_user != null)
+            {
+                name_user.Content = _user.fio_user;
+                phone_user.Content = "Телефон: " + _user.phone_num;
+            }
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 0;
+            opgridAnimation.To = 1;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.4);
+            border.BeginAnimation(StackPanel.OpacityProperty, opgridAnimation);
+        }
+
+
+
+        private void Click_redact(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Anim_move(MainWindow.main.scroll_main, MainWindow.main.frame_main, MainWindow.main.frame_main, new Pages.PagesUser.User_win(user_loc));
+        }
+
+        private void Click_remove(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainWindow.connect.LoadData(ClassConnection.Connection.tables.users);
+
+                Call userFind = MainWindow.connect.calls.Find(x => x.user_id == user_loc.id);
+
+                if (userFind != null)
+                {
+                    var click = MessageBox.Show("У данного клиента есть звонки, все равно удалить его?", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                    if (click == MessageBoxResult.No)
+                    {
+                        return;
+                    }
+                }
+
+                string vs1 = $"DELETE FROM [calls] WHERE [user_id] = '{user_loc.id.ToString()}'";
+                var pc1 = MainWindow.connect.QueryAccess(vs1);
+
+                string vs = "DELETE FROM [users] WHERE [Код] = " + user_loc.id.ToString() + "";
+                var pc = MainWindow.connect.QueryAccess(vs);
+                if (pc != null && pc1 != null)
+                {
+                    MessageBox.Show("Успешное удаление клиента", "Успешное", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MainWindow.connect.LoadData(ClassConnection.Connection.tables.users);
+                    MainWindow.main.Anim_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.users);
+                }
+                else MessageBox.Show("Запрос на удаление клиента не был обработан", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
